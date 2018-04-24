6d38e472552e17d92fc4a9f53db749e0d3431399
Jason Smyth <smyt0058@algonquinlive.com>
2018-04-24 19:16:27 -0400

    min/target/compiled sdk change
    - changed onBoarder module min sdk to 23, target/compiled to 27
    - made msgEventpost method in ChatActivity static, relevant members also
    - added journal_thanks message

--- Oso/onboarder/build/generated/source/r/debug/android/support/v7/cardview/R.java
+++ Oso/onboarder/build/generated/source/r/debug/android/support/v7/cardview/R.java
@@ -8,37 +8,38 @@ package android.support.v7.cardview;
 
 public final class R {
     public static final class attr {
-        public static int cardBackgroundColor = 0x7f04003e;
-        public static int cardCornerRadius = 0x7f04003f;
-        public static int cardElevation = 0x7f040040;
-        public static int cardMaxElevation = 0x7f040041;
-        public static int cardPreventCornerOverlap = 0x7f040042;
-        public static int cardUseCompatPadding = 0x7f040043;
-        public static int contentPadding = 0x7f04005b;
-        public static int contentPaddingBottom = 0x7f04005c;
-        public static int contentPaddingLeft = 0x7f04005d;
-        public static int contentPaddingRight = 0x7f04005e;
-        public static int contentPaddingTop = 0x7f04005f;
+        public static int cardBackgroundColor = 0x7f040045;
+        public static int cardCornerRadius = 0x7f040046;
+        public static int cardElevation = 0x7f040047;
+        public static int cardMaxElevation = 0x7f040048;
+        public static int cardPreventCornerOverlap = 0x7f040049;
+        public static int cardUseCompatPadding = 0x7f04004a;
+        public static int cardViewStyle = 0x7f04004b;
+        public static int contentPadding = 0x7f040065;
+        public static int contentPaddingBottom = 0x7f040066;
+        public static int contentPaddingLeft = 0x7f040067;
+        public static int contentPaddingRight = 0x7f040068;
+        public static int contentPaddingTop = 0x7f040069;
     }
     public static final class color {
-        public static int cardview_dark_background = 0x7f06002d;
-        public static int cardview_light_background = 0x7f06002e;
-        public static int cardview_shadow_end_color = 0x7f06002f;
-        public static int cardview_shadow_start_color = 0x7f060030;
+        public static int cardview_dark_background = 0x7f06002c;
+        public static int cardview_light_background = 0x7f06002d;
+        public static int cardview_shadow_end_color = 0x7f06002e;
+        public static int cardview_shadow_start_color = 0x7f06002f;
     }
     public static final class dimen {
-        public static int cardview_compat_inset_shadow = 0x7f08004d;
-        public static int cardview_default_elevation = 0x7f08004e;
-        public static int cardview_default_radius = 0x7f08004f;
+        public static int cardview_compat_inset_shadow = 0x7f08004e;
+        public static int cardview_default_elevation = 0x7f08004f;
+        public static int cardview_default_radius = 0x7f080050;
     }
     public static final class style {
-        public static int Base_CardView = 0x7f160009;
-        public static int CardView = 0x7f16009e;
-        public static int CardView_Dark = 0x7f16009f;
-        public static int CardView_Light = 0x7f1600a0;
+        public static int Base_CardView = 0x7f16000b;
+        public static int CardView = 0x7f1600a0;
+        public static int CardView_Dark = 0x7f1600a1;
+        public static int CardView_Light = 0x7f1600a2;
     }
     public static final class styleable {
-        public static int[] CardView = { 0x01010140, 0x0101013f, 0x7f04003e, 0x7f04003f, 0x7f040040, 0x7f040041, 0x7f040042, 0x7f040043, 0x7f04005b, 0x7f04005c, 0x7f04005d, 0x7f04005e, 0x7f04005f };
+        public static int[] CardView = { 0x01010140, 0x0101013f, 0x7f040045, 0x7f040046, 0x7f040047, 0x7f040048, 0x7f040049, 0x7f04004a, 0x7f040065, 0x7f040066, 0x7f040067, 0x7f040068, 0x7f040069 };
         public static int CardView_android_minHeight = 0;
         public static int CardView_android_minWidth = 1;
         public static int CardView_cardBackgroundColor = 2;
