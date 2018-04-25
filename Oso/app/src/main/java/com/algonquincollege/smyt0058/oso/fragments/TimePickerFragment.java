b7de095c60cc8d19efe38bcc06bbbdeb83c2f2b1
Jason Smyth <smyt0058@algonquinlive.com>
2018-04-24 23:31:06 -0400

    FINAL MERGE!... before cleanup

--- Oso/app/src/main/java/com/algonquincollege/smyt0058/oso/fragments/TimePickerFragment.java
+++ Oso/app/src/main/java/com/algonquincollege/smyt0058/oso/fragments/TimePickerFragment.java
@@ -3,14 +3,16 @@ package com.algonquincollege.smyt0058.oso.fragments;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.TimePickerDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.algonquincollege.smyt0058.oso.notifications.Notification;
 import com.algonquincollege.smyt0058.oso.util.api.SharedPrefUtils;
 
-import 	java.util.Calendar;
+import java.util.Calendar;
 
 public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
 
@@ -29,6 +31,25 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
         // Do something with the time chosen by the user
         SharedPrefUtils.putSettingsState(getContext(), hourOfDay, minute );
+        SharedPreferences prefs = SharedPrefUtils.getAppState(getContext());
+        long next = prefs.getLong(SharedPrefUtils.NEXT_QUESTIONNAIRE_DATE, 0);
+
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(next);
+        //long now = c.getTimeInMillis();
+
+        // set hour and minute.
+        // adjust to tomorrow if that result is not in the future
+        // (by five seconds minute at least)
+        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        c.set(Calendar.MINUTE, minute);
+        next = c.getTimeInMillis();
+        SharedPrefUtils.putNextDateState(getContext(), next);
+//        if (c.getTimeInMillis() < now + 5 * 1000) {
+//            c.setTimeInMillis(c.getTimeInMillis() + 24 * 60 * 60 * 1000);
+//        }
+
+        Notification.init(getContext());
         Toast.makeText(getContext(), "Time Picked: " + hourOfDay + ":" + minute, Toast.LENGTH_SHORT).show();
     }
 
